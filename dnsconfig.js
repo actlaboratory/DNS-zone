a2b43b340fc32b6e6597bd643c3ab15c8a330014
guredora <contact@guredora.com>
2024-02-17 17:14:42 +0900

    Initial commit

---
+++ dnsconfig.js
@@ -0,0 +1,43 @@
+// @ts-check
+/// <reference path="types-dnscontrol.d.ts" />
+
+var REG_NONE = NewRegistrar("none");    // No registrar.
+var DNS_BIND = NewDnsProvider("bind");  // ISC BIND.
+
+// Domains:
+
+DEFAULTS(
+    DefaultTTL("1m")
+);
+
+D("actlab.org", REG_NONE, DnsProvider(DNS_BIND),
+    A("@", "160.251.151.230"),
+    A("autoconfig", "160.251.151.230"),
+    A("www", "160.251.151.230"),
+    A("lamp", "160.16.58.123"),
+    A("www.lamp", "160.16.58.123"),
+    A("stg", "160.251.151.157"),
+    A("www.stg", "160.251.151.157"),
+    MX("lamp", 5, "salmoncoyote7.sakura.ne.jp."),
+    MX("www.lamp", 5, "salmoncoyote7.sakura.ne.jp."),
+    MX("@", 10, "@"),
+    MX("sendgrid", 10, "mx.sendgrid.net."),
+    MX("mail", 1, "aspmx.l.google.com."),
+    MX("mail", 5, "ALT1.ASPMX.L.GOOGLE.COM."),
+    MX("mail", 10, "ALT3.ASPMX.L.GOOGLE.COM."),
+    MX("mail", 10, "ALT4.ASPMX.L.GOOGLE.COM."),
+    TXT("@", "v=spf1 ip4:160.251.151.230 ip4:160.251.151.157 ~all"),
+    TXT("@", "10de176aa2"),
+    TXT("*", "10de176aa2"),
+    TXT("@", "google-site-verification=5-Av1k3BxIDJ_nqzZ2F7h2T37Ex7jIMOzeWIR7_ErHw"),
+    TXT("*", "google-site-verification=5-Av1k3BxIDJ_nqzZ2F7h2T37Ex7jIMOzeWIR7_ErHw"),
+    TXT("*", "actlaboratory"),
+    TXT("@", "google-site-verification=ho7hHpbeIFf_lGd6l9QX4PT3deKKfbzEKA1wbT1E9mw"),
+    TXT("*", "google-site-verification=ho7hHpbeIFf_lGd6l9QX4PT3deKKfbzEKA1wbT1E9mw"),
+    TXT("*", "google-site-verification=esxrTlmStDPUVGkDvO4qSsmWvji3-f50AfW-psXrIgo"),
+    TXT("@", "google-site-verification=esxrTlmStDPUVGkDvO4qSsmWvji3-f50AfW-psXrIgo"),
+    TXT("sendgrid", "v=spf1 include:sendgrid.net ~all"),
+    TXT("sg._domainkey", "k=rsa; t=s; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCnjex8a/J19j4cP79OIrB7FccRAjdU4F32o3uGprX4ukE/gLkUZlFAXPqnC0cOcrWhjjem3wDpej/Co9TT99FKANthTO3Jn/NCwqd7c3TS+UsHR4atTFrQagdU4/09DceoOZ2Fx0ygHdVJdCd6vaIlXoSL/nHcR+WBAbaMkqiDawIDAQAB"),
+    CNAME("click", "sendgrid.net."),
+    CNAME("37371635", "sendgrid.net.")
+);
